a81c03a6afc8367c4a5d4a17741121c07ca4d5f5
Cristina Padilla <crispitipina@gmail.com>
2022-10-06 21:05:53 +0200

    Create & access props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
-
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+  ];
+
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
     </div>
   );
 }
